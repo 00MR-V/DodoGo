f898471a9519b95d033aa1efe38409f5944b91d2
Luvraj Kaundun <luvrajkaundun@gmail.com>
2025-08-17 22:21:42 +0400

    fix: wrap ResetPasswordForm in Suspense

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,14 +1,18 @@
-import Link from "next/link";
+"use client";
 
-import { ResetPasswordForm } from "@/components/reset-password-form";
+import { Suspense } from "react";
+import Link from "next/link";
 import Image from "next/image";
 
-export default function LoginPage() {
+import { ResetPasswordForm } from "@/components/reset-password-form";
+
+export default function ResetPasswordPage() {
     return (
         <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
-
-                <ResetPasswordForm />
+                <Suspense fallback={<div>Loading reset password...</div>}>
+                    <ResetPasswordForm />
+                </Suspense>
             </div>
         </div>
     );
